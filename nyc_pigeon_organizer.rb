c944733f1a0d4a964f4e49508ca675940140494b
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-25 17:42:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_pigeons = Hash.new
+  data.each do |(key, value)|
+    value.each do |(attribute, names)|
+      names.each do |name|
+        if !organized_pigeons.has_key?(name)
+          organized_pigeons[name] = {}
+        end
+        if !organized_pigeons[name].has_key?(key)
+          organized_pigeons[name][key] = ["#{attribute}"]
+        else
+          organized_pigeons[name][key] << "#{attribute}"
+        end
+      end
+    end
+  end
+organized_pigeons
 end
