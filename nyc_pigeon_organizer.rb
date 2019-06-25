282dd427c86b208f0f3ab63627bb4473f2668d95
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-25 16:59:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
     value.each do |(attribute, names)|
       names.collect do |name|
         if !sorted_names.any?(name)
-          organized_pigeons["#{name}"]
+          organized_pigeons["#{name}"] = {}
           if names.any?(name)
             sorted_names << attribute
             binding.pry
