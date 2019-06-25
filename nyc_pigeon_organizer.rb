89d51e5e715da313b96f7eefea37b8e17aef0cfb
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-25 17:39:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
         if !organized_pigeons.has_key?(name)
           organized_pigeons[name] = {}
         end
-        if !organized_pigeons[name].has_key(key)
+        if !organized_pigeons[name].has_key?(key)
           organized_pigeons[name][key] = ["#{attribute}"]
         else
           organized_pigeons[name][key] << "#{attribute}"
