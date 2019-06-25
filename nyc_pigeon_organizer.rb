c7d39041e0b134d623dd97f3f5a3226a859d799f
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-25 16:22:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ sorted_names = []
 def nyc_pigeon_organizer(data)
   data.each do |(key, value)|
     value.each do |(attribute, names)|
-      sorted_names = names.collect do |name|
+      names.collect do |name|
         if !sorted_names.any?(name)
           sorted_names << name
         end
