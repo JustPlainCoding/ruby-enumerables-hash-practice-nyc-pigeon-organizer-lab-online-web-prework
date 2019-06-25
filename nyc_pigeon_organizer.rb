e7652712862499d2073a3c76a209eeb0682d850a
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-25 16:58:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,12 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   sorted_names = []
+  organized_pigeons = Hash.new
   data.each do |(key, value)|
     value.each do |(attribute, names)|
       names.collect do |name|
         if !sorted_names.any?(name)
-          sorted_names << name
+          organized_pigeons["#{name}"]
           if names.any?(name)
             sorted_names << attribute
             binding.pry
