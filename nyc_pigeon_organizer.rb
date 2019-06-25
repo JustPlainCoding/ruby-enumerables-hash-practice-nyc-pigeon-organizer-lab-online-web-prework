6312a2debf0b3cde6f71fa1117447691aa3ac5ac
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-25 17:39:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,18 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
-  sorted_names = []
   organized_pigeons = Hash.new
   data.each do |(key, value)|
     value.each do |(attribute, names)|
       names.collect do |name|
-        if !sorted_names.any?(name)
-          if names.any?(name)
-            if organized_pigeons[name].has_key?(key)
-              organized_pigeons[name][key] << "#{attribute}"
-            else
-              organized_pigeons[name][key] = ["#{attribute}"]
-            end
-          end
+        if !organized_pigeons.has_key?(name)
+          organized_pigeons[name] = {}
         end
-      end
+        if !organized_pigeons[name].has_key(key)
+          organized_pigeons[name][key] = ["#{attribute}"]
+        else
+          organized_pigeons[name][key] << "#{attribute}"
+        end
     end
   end
 organized_pigeons
