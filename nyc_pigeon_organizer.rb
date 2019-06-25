8ceab4ffc1273243d9af3999e17a77d6227cc239
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-25 16:18:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |(key, value)|
     value.each do |(attribute, names)|
       names.collect do |name|
-        if !sorted_names.has_key?(name)
+        if !sorted_names.any?(name)
           sorted_names << name
         end
     end
